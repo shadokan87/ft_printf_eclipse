a9bbe76f7b84fa41268f908dcfeaea80c9d36e42
Eclipse TOURE <motoure@e1r3p9.42.fr>
2021-07-19 15:58:08 +0200

    ok

--- py_printf.py
+++ py_printf.py
@@ -2,6 +2,8 @@ import subprocess
 import os
 import sys
 import random
+from random import seed
+from random import randint
 
 header="""                                                                              
  __________________________________________________________________________________________
@@ -31,6 +33,19 @@ _____________|__| _____/  |__/ ____\            _/  |_  ____   _______/  |_  ___
 /_____/_____/_____/_____/_____/_____/_____/_____/_____/_____/_____/_____/_____/_____/      
 """
 ENDL = "\n"
+#utils
+def rand_int_arr(min, max, amount):
+    i = 0
+    tmp = max
+    val = list(range(0, amount))
+    while (i < amount):
+        val[i] = str(randint(min, max))
+        max /= randint(1, 10)
+        if (max <= 1):
+            max = tmp
+        i += 1
+    return (val)
+#
 def execCmd(prog, concat):
 	proc=subprocess.Popen(prog + " " + concat, shell=True, stdout=subprocess.PIPE)
 	output=proc.communicate()[0]
@@ -43,18 +58,22 @@ class compose:
         self.body = body
     def funcInline(self, funcname, params):
         res = ""
-        res += funcname + "("
+        res += "\t" + funcname + "("
         i = 0
         while (i < len(params)):
             res += params[i]
             if (i != len(params) - 1):
-                res += ","
+                res += ", "
             i += 1
-            res += ");\n"
-            return (res)
+        res += ");\n"
+        return (res)
     def startFunc(self, ret, funcname, params):
         inline = self.funcInline(funcname, params)
-        self.body += ret + "\t" + str(inline) + "{" + ENDL
+        concat = ""
+        for elem in inline:
+            if (elem != ";"):
+                concat += elem
+        self.body += ret + "\t" + str(concat) + "{" + ENDL
     #fill compose
     def Header(self, elems):
         for elem in elems:
@@ -68,7 +87,16 @@ class compose:
         for elem in elems:
             self.body += elem
         self.body += ";" + ENDL
+    def varInitAssign(self, elems, val):
+        self.body += "\t"
+        for elem in elems:
+            self.body += elem
+        self.body += " = " + str(val) + ";" + ENDL
+
     def varAssign(self, elems):
+        self.body += "\t"
+        for elem in elems:
+            self.body += elem
         self.body += elems[0] + " " + "=" + " " + elems[1]
     def __print__(self):
         print(self.headers)
@@ -80,23 +108,59 @@ class compose:
         self.body += self.funcInline(funcname, params)
     def endFunc(self):
         self.body += "}"
-    def spanVar(self, elems, amount):
-        self.varInit(elems)
+    def spawnVar(self, elems, amount):
+        i = 0
+        concat = []
+        while (i < amount):
+            concat = elems[0]
+            concat += str(elems[1])+ str(i)
+            self.varInit(concat)
+            i += 1
+    def spawnVarAssign(self, elems, val):
+        i = 0
+        concat = []
+        while (i < len(val)):
+            concat = elems[0]
+            concat += str(elems[1]) + str(i)
+            self.varInitAssign(concat, val[i])
+            i += 1
+def prep_dp(orig):
+    orig.Define(["$MOD_D_POSITIVE int"])
+    orig.startFunc("void", "__d", ["void"])
+    orig.spawnVarAssign(["$MOD_D_POSITIVE ", "_dp"], rand_int_arr(0, 2147483647,30))
+    orig.body += ENDL
+    i = 0
+    while (i < 30):
+        orig.body += orig.funcInline("printf", ["\"%s\"", "_dp" + str(i)])
+        i += 1
+    orig.endFunc()
+def prep_np(orig):
+    orig.Define(["$MOD_D_NEGATIVE int"])
+    orig.startFunc("void", "__d", ["void"])
+    orig.spawnVarAssign(["$MOD_D_POSITIVE ", "_np"], rand_int_arr(0, 2147483647,30))
+    orig.body += ENDL
+    i = 0
+    while (i < 30):
+        orig.body += orig.funcInline("printf", ["\"%s\"", "_np" + str(i)])
+        i += 1
+    orig.endFunc()
+
+    #orig.Define(["$MOD_D_NEG int"])
+    #orig.Define(["$MOD_S char *"])
+    #orig.Define(["$MOD_C char"])
+    #orig.Define(["$MOD_U unsigned int"])
 
 def prep_test():
     #composet test
     orig = compose()
     orig.Header(["<stdio.h>","<stdlib.h>"])
-    orig.startFunc("int", "main", ["void"])
-    orig.varInit(["int *", "test"])
-    orig.spanVar(["int *"])
-    orig.endFunc()
+    prep_dp(orig)
     orig.__print__()
     #
-    #prog_name = "p_p_cbuild"
-    #main_fd = open("./py_printf_main.c", "w+")
-    #main_fd.write(main_file)
-   # execCmd("cc", "*.c" + " -o " + prog_name)
+    prog_name = "p_p_cbuild"
+    main_fd = open("./py_printf_main.c", "w+")
+    main_fd.write(orig.headers + orig.defines + orig.body)
+    execCmd("cc", "*.c" + " -o " + prog_name)
     return ("null")
 
 def main():
---
+++ py_printf_main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+# define $MOD_D_POSITIVE int
+void		__d(void)
+{
+	$MOD_D_POSITIVE _dp0 = 1164758562;
+	$MOD_D_POSITIVE _dp1 = 46668524;
+	$MOD_D_POSITIVE _dp2 = 150029855;
+	$MOD_D_POSITIVE _dp3 = 56591977;
+	$MOD_D_POSITIVE _dp4 = 28241149;
+	$MOD_D_POSITIVE _dp5 = 233680;
+	$MOD_D_POSITIVE _dp6 = 124572;
+	$MOD_D_POSITIVE _dp7 = 119877;
+	$MOD_D_POSITIVE _dp8 = 46788;
+	$MOD_D_POSITIVE _dp9 = 27363;
+	$MOD_D_POSITIVE _dp10 = 5937;
+	$MOD_D_POSITIVE _dp11 = 408;
+	$MOD_D_POSITIVE _dp12 = 225;
+	$MOD_D_POSITIVE _dp13 = 6;
+	$MOD_D_POSITIVE _dp14 = 7;
+	$MOD_D_POSITIVE _dp15 = 8;
+	$MOD_D_POSITIVE _dp16 = 0;
+	$MOD_D_POSITIVE _dp17 = 2093748362;
+	$MOD_D_POSITIVE _dp18 = 87324325;
+	$MOD_D_POSITIVE _dp19 = 15557136;
+	$MOD_D_POSITIVE _dp20 = 53084447;
+	$MOD_D_POSITIVE _dp21 = 28504011;
+	$MOD_D_POSITIVE _dp22 = 1814250;
+	$MOD_D_POSITIVE _dp23 = 406709;
+	$MOD_D_POSITIVE _dp24 = 28528;
+	$MOD_D_POSITIVE _dp25 = 16365;
+	$MOD_D_POSITIVE _dp26 = 483;
+	$MOD_D_POSITIVE _dp27 = 253;
+	$MOD_D_POSITIVE _dp28 = 275;
+	$MOD_D_POSITIVE _dp29 = 40;
+
+	printf("%s", _dp0);
+	printf("%s", _dp1);
+	printf("%s", _dp2);
+	printf("%s", _dp3);
+	printf("%s", _dp4);
+	printf("%s", _dp5);
+	printf("%s", _dp6);
+	printf("%s", _dp7);
+	printf("%s", _dp8);
+	printf("%s", _dp9);
+	printf("%s", _dp10);
+	printf("%s", _dp11);
+	printf("%s", _dp12);
+	printf("%s", _dp13);
+	printf("%s", _dp14);
+	printf("%s", _dp15);
+	printf("%s", _dp16);
+	printf("%s", _dp17);
+	printf("%s", _dp18);
+	printf("%s", _dp19);
+	printf("%s", _dp20);
+	printf("%s", _dp21);
+	printf("%s", _dp22);
+	printf("%s", _dp23);
+	printf("%s", _dp24);
+	printf("%s", _dp25);
+	printf("%s", _dp26);
+	printf("%s", _dp27);
+	printf("%s", _dp28);
+	printf("%s", _dp29);
+}
